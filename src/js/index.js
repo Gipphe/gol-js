91f5f497b5b017156f311acb1b906f36fb4b3696
Victor Nascimento Bakke <gipphe@gmail.com>
2017-02-01 15:00:58 +0100

    More work

--- src/js/index.js
+++ src/js/index.js
@@ -8,17 +8,35 @@ for (var i = 0; i < 100; i++) {
 	grid.push(row);
 }
 
-grid.forEach(function(row, y) {
-	var cell;
-	row.forEach(function(cell, x) {
-		cell = document.createElement('DIV');
-		cell.classList.add(cell);
-		cell.setAttribute('data-x', x);
-		cell.setAttribute('data-y', y);
-		body.appendChild(cell);
+var display = function() {
+	grid.forEach(function(row, y) {
+		var cell;
+		row.forEach(function(cell, x) {
+			cell = document.createElement('DIV');
+			cell.classList.add('cell');
+			cell.setAttribute('data-x', x);
+			cell.setAttribute('data-y', y);
+			body.appendChild(cell);
+		});
 	});
-});
+};
 
 body.addEventListener('click', function(e) {
-
+	var cell = e.target;
+	if (!e.classList.contains('cell')) {
+		return;
+	}
+	cell.classList.add('alive');
 });
+
+var loop = function() {
+	var oldGrid = grid.slice();
+	oldGrid.forEach(function(row, y) {
+		var cell;
+		row.forEach(function(cell, x) {
+			console.log(cell, x, y);
+		});
+	});
+
+	setTimeout(loop, 500);
+};
